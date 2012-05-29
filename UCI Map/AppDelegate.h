f72078d7fc879ef383b8551c44c39407c8272c4d
Booth <booth@Campus-Rep-MacBook-Pro.local>
2012-05-28 19:28:36 -0700

    Initial Commit

---
+++ UCI Map/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  UCI Map
+//
+//  Created by John Appleseed on 5/28/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ UCI Map/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  UCI Map
+//
+//  Created by John Appleseed on 5/28/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
